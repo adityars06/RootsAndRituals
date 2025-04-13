6690d2d309c429c1ab621f24aa37da931cb72ba2
Aditya Raj Singh <aditya@Adityas-MacBook-Air-3.local>
2025-04-13 16:53:21 +0530

    tried out routing and created appbar and optimised using lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState ,useEffect} from 'react'
+import { useState ,useEffect,lazy} from 'react'
+import { BrowserRouter, Route, Routes, useNavigate, useNavigation } from 'react-router-dom'
 import './App.css'
 import SearchBar from './components/SearchBar'
-import ListingPage from './components/ListingPage'
+const ListingPage= lazy(()=>import('./components/ListingPage')) 
 import { productfetch } from './api/cart'
+const Cart = lazy(()=>import('./components/Cart')) 
+import React from 'react'
 
 function App() {
   const [products,setProducts]= useState([])
@@ -10,16 +13,37 @@ function App() {
   const [value,setValue]=useState("");
 
   useEffect(()=>{
-    productfetch().then((res)=>{setProducts(res)
-       setFilteredList(res)})
+    productfetch().then((res)=>{
+      setProducts(res)
+       setFilteredList(res)
+    })
   },[])
 
   
 
   return (
     <div>
-     <SearchBar products={products} filteredList={filteredList} setFilteredList={setFilteredList} value={value} setValue={setValue} />
-     <ListingPage filteredList={filteredList} products={products} />
+    <BrowserRouter>
+    <AppBar/>
+    <SearchBar products={products} filteredList={filteredList} setFilteredList={setFilteredList} value={value} setValue={setValue} />
+      <Routes>
+        <Route path='/' element={<ListingPage filteredList={filteredList} products={products} />} />
+        <Route path='/cart' element={<Cart/>} />
+        
+        
+      </Routes>
+    </BrowserRouter>
+     
+    </div>
+  )
+}
+
+function AppBar(){
+  const navigate = useNavigate()
+  return(
+    <div style={{border:"2px solid black"}}>
+      <button onClick={()=>navigate('/')}>ListingPage</button>
+      <button onClick={()=>navigate('/cart')}>Cart</button>
     </div>
   )
 }
